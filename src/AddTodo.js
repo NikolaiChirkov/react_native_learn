abf60d643b3a05c60cc0c9741aa51e70549b2512
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-11-04 18:53:58 +0300

    add styles for addTodo

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -4,14 +4,23 @@ import { View, StyleSheet, TextInput, Button } from 'react-native';
 export const AddTodo = props => {
     return (
         <View style={styles.block}>
-            <TextInput />
-            <Button />
+            <TextInput style={styles.input}/>
+            <Button title="Добавить"/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     block: {
-
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }, 
+    input: {
+        width: '70%',
+        padding: 10,
+        borderStyle: "solid",
+        borderBottomWidth: 2,
+        borderBottomColor: '#3949ab'
     }
 })
