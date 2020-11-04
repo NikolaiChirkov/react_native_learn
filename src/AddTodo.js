32461b7a87fdc993ecfc9893f2f198930cab3d35
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-11-04 18:44:36 +0300

    setup Todo body

---
+++ src/AddTodo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet, TextInput, Button } from 'react-native';
+
+export const AddTodo = props => {
+    return (
+        <View style={styles.block}>
+            <TextInput />
+            <Button />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    block: {
+
+    }
+})
